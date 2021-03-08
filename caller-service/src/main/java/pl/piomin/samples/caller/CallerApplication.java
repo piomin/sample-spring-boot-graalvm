6c67eb52c5499a9aa2b52f110ce0cac063749c99
piomin <piotr.minkowski@gmail.com>
2021-03-08 14:07:51 +0100

    adding buildpacks + new version of spring-native

--- caller-service/src/main/java/pl/piomin/samples/caller/CallerApplication.java
+++ caller-service/src/main/java/pl/piomin/samples/caller/CallerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(proxyBeanMethods = false, exclude = SpringDataWebAutoConfiguration.class)
+@SpringBootApplication
 public class CallerApplication {
 
     public static void main(String[] args) {
--- callme-service/src/main/java/pl/piomin/samples/callme/CallmeApplication.java
+++ callme-service/src/main/java/pl/piomin/samples/callme/CallmeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 
-@SpringBootApplication(proxyBeanMethods = false, exclude = SpringDataWebAutoConfiguration.class)
+@SpringBootApplication
 public class CallmeApplication {
 
     public static void main(String[] args) {
