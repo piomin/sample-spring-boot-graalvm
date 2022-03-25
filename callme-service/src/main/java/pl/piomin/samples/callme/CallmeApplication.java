6791171f748c1bb4f6d18c0e02f07f36fdbc2e0a
piomin <piotr.minkowski@gmail.com>
2022-03-25 10:36:44 +0100

    update

--- callme-service/src/main/java/pl/piomin/samples/callme/CallmeApplication.java
+++ callme-service/src/main/java/pl/piomin/samples/callme/CallmeApplication.java
@@ -2,7 +2,6 @@ package pl.piomin.samples.callme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 
 @SpringBootApplication
 public class CallmeApplication {
--- callme-service/src/main/java/pl/piomin/samples/callme/model/Callme.java
+++ callme-service/src/main/java/pl/piomin/samples/callme/model/Callme.java
@@ -1,24 +1,48 @@
 package pl.piomin.samples.callme.model;
 
-import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class Callme {
 
     @Id
     @GeneratedValue
     private Integer id;
     @Temporal(TemporalType.TIMESTAMP)
-    @NonNull
     private Date addDate;
-    @NonNull
     private String podName;
 
+    public Callme() {
+    }
+
+    public Callme(Date addDate, String podName) {
+        this.addDate = addDate;
+        this.podName = podName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+
+    public String getPodName() {
+        return podName;
+    }
+
+    public void setPodName(String podName) {
+        this.podName = podName;
+    }
 }
