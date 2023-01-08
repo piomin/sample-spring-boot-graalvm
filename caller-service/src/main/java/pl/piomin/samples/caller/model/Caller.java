1e7ae9a8c1d7940eddf549242f771f147b59da96
piomin <piotr.minkowski@gmail.com>
2023-01-08 23:14:32 +0100

    remove lombok + add renovate.json

--- caller-service/src/main/java/pl/piomin/samples/caller/model/Caller.java
+++ caller-service/src/main/java/pl/piomin/samples/caller/model/Caller.java
@@ -1,24 +1,47 @@
 package pl.piomin.samples.caller.model;
 
-import lombok.*;
-
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class Caller {
 
     @Id
     @GeneratedValue
     private Integer id;
     @Temporal(TemporalType.TIMESTAMP)
-    @NonNull
     private Date addDate;
-    @NonNull
     private String podName;
 
+    public Caller() {
+    }
+
+    public Caller(Date addDate, String podName) {
+        this.addDate = addDate;
+        this.podName = podName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+
+    public String getPodName() {
+        return podName;
+    }
+
+    public void setPodName(String podName) {
+        this.podName = podName;
+    }
 }
